be25c4a3e0ffc8e7c537fabb7c1a4b962aab63b6
Maurizio <maurizio.faedda1@gmail.com>
2021-01-20 14:40:11 +0100

    migration created and smartphones table defined

---
+++ database/migrations/2021_01_20_132254_create_smartphones_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSmartphonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('smartphones', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 20);
+            $table->string('model', 30);
+            $table->smallInteger('memory');
+            $table->tinyInteger('ram');
+            $table->smallInteger('battery');
+            $table->string('color', 20);
+            $table->float('price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('smartphones');
+    }
+}
