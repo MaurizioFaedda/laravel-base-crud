b3e7e9aaca0210851a3917452f0fc1ccdf5791d5
Maurizio <maurizio.faedda1@gmail.com>
2021-01-21 16:36:45 +0100

    accessories table added

---
+++ database/migrations/2021_01_21_153031_create_accessories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccessoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accessories', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 20);
+            $table->string('categories', 50);
+            $table->float('price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accessories');
+    }
+}
